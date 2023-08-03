20e1c92cb6b751e01be575e94c5d52fa0e9bf1fd
VS <anaberrantis@gmail.com>
2023-08-03 23:41:45 +0200

    Models from blender added

--- components/3D.js
+++ components/3D.js
@@ -2,32 +2,16 @@ import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, TorusKnot } from "@react-three/drei";
 import * as THREE from "three";
+// import Model from './MiniModelMat';
 
 // raycast
-
-const raycaster = new THREE.Raycaster();
-const pointer = new THREE.Vector2();
-
-const onMouseMove = (event) => {
-  // calculate pointer position in normalized device coordinates
-	// (-1 to +1) for both components
-
-	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-
-  raycaster.setFromCamera(pointer, test.camera);
-  const intersects = raycaster.intersectObjects(test.scene.children);
-}
 
 const Hero = () => {
   return(
     <Canvas>
-        <OrbitControls enableZoom={false} autoRotate autoRotateSpeed={3}/>
-        {/* <TorusKnot args={[10, 3, 100, 10]} scale={0.13}>
-            <meshNormalMaterial wireframe={true}/>
-        </TorusKnot> */}
-        <TorusKnot args={[10, 3, 100, 10]} scale={0.15}>
-            <meshNormalMaterial wireframe={false} flatShading={true}/>
+        <TorusKnot args={[10, 3, 100, 10]} position={[0,0,1]}scale={0.15}>
+            <OrbitControls enableZoom={false} autoRotate autoRotateSpeed={8}/>
+            <meshBasicMaterial wireframe={true} flatShading={true} color='#eab308'/>
         </TorusKnot>
     </Canvas>
   );
